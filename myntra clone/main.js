a9805cb45fa6c7f7dfc5abe6145c9831e1d052de
Kavya V <kavya0002u@gmail.com>
2025-08-02 22:20:51 +0530

    Delete myntra clone directory

--- myntra clone/main.js
+++
@@ -1,9 +0,0 @@
-let shop=document.getElementById("shopmen");
-console.log(shop);
-let generateshop=()=>{
-    return (shop.innerHTML=`
-    <div>
-    <a href="smartwatch.html"><img class="minimain" src="1mini.jpg"></a>
-    </div>`)
-};
-generateshop();
